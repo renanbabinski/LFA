98522728eec0645c10462956797934e72ddd1807
renanbabinski <renan10babinski@hotmail.com>
2022-04-08 01:23:10 -0300

    carga de tokens finalizada

--- trabalho_lfa/extensions.py
+++ trabalho_lfa/extensions.py
@@ -23,7 +23,7 @@ def gen_alpha():
             alpha.append('EMPTY')
 
 
-def increment(letter:str()) -> str():
+def increment(letter:str) -> str:
     letter = chr(ord(letter) + 1)
     return letter
 
@@ -31,17 +31,62 @@ def increment(letter:str()) -> str():
 
 class Afnd:
     def __init__(self) -> None:
-        self.afnd_table = pd.DataFrame(['S'], columns=['Ø'])
+        self.afnd_table = pd.DataFrame(["->S'"], columns=['Ø'])
+        self.afnd_table.set_index('Ø', drop=True, inplace=True)
         self.iterator = gen_alpha()
 
     def show_table(self):
-        print(self.afnd_table.to_markdown(index=False))
+        print(self.afnd_table.to_markdown(index=True))
 
-    def append_row(self, data=[]):
-        row = pd.DataFrame([f'{next(self.iterator)}'], columns=['Ø'])
-        self.afnd_table = pd.concat([self.afnd_table, row], ignore_index=True)
+    def append_row(self, terminal_column=None, current_state=None, next_state=None):
+        if terminal_column:
+            data = {
+                'Ø': [current_state],
+                terminal_column: [next_state]
+            }
+        else:
+            data = {
+                'Ø': [f"{current_state}*"]
+            }
+        row = pd.DataFrame(data)
+        row.set_index('Ø', drop=True, inplace=True)
+        print("APPEND!")
+        print(row.to_markdown())
+        self.afnd_table = pd.concat([self.afnd_table, row])
 
-    
+    def terminal_insert_head(self, terminal:str, current_state:str) ->str:
+        if terminal not in self.afnd_table.columns:
+            print(f"Terminal {terminal} not in columns!")
+            self.afnd_table.loc["->S'", [terminal]] = current_state
+            return current_state
+        else:
+            print(f"Terminal {terminal} is in columns!")
+            if self.afnd_table[terminal].isnull()["->S'"]:
+                self.afnd_table.loc["->S'", [terminal]] = current_state
+            else:
+                self.afnd_table.loc["->S'", [terminal]] += ',' + current_state
+            return current_state
+
+    def terminal_insert_middle(self, terminal:str, current_state:str):
+        print(f"Terminal {terminal} not in columns!")
+        next_state = next(self.iterator)
+        self.append_row(terminal, current_state, next_state)
+        return next_state
+        
+
+    def terminal_insert_tail(self, terminal:str, current_state:str):
+            next_state = next(self.iterator)
+            self.append_row(terminal, current_state, next_state)
+            current_state = next_state
+            next_state = next(self.iterator)
+            self.append_row(current_state=current_state)
+            return next_state
+
+    def fill_na_values(self):
+        self.afnd_table =  self.afnd_table.fillna('')
+
+
+
 class File:
     def __init__(self, file_path) -> None:
         with open(file_path, 'r', encoding='utf-8') as file:
--- trabalho_lfa/lfa.py
+++ trabalho_lfa/lfa.py
@@ -4,7 +4,6 @@
 
 import pandas as pd
 from extensions import Afnd, File
-import os
 
 file_path = "trabalho_lfa/input.txt"
 
@@ -12,20 +11,41 @@ file_path = "trabalho_lfa/input.txt"
 def main():
     file = File(file_path)
     # file.show_file_lines()
-    
-    load_tokens(file)
-
 
     afnd = Afnd()
-    afnd.show_table()
-    for i in range(100):
-        afnd.append_row()
+    # afnd.show_table()
+    
+    load_tokens(file, afnd)
+    # afnd.append_row('e', 'A', 'B')
+
+    # print(afnd.afnd_table['e'].isnull()["->S'"])
+
+    afnd.fill_na_values()
+    print('\n\n')
     afnd.show_table()
 
+    
 
-def load_tokens(file:File):
+
+    
+    
+def load_tokens(file:File, afnd:Afnd):
+    next_state = next(afnd.iterator)
     for line in file.get_line():
-        print(line.strip())
+        line = line.strip()
+        for i, terminal in enumerate(line):
+            if i == 0:                # first terminal from token
+                next_state = afnd.terminal_insert_head(terminal, current_state=next_state)
+                # print(next_state)
+            elif i == len(line) - 1:  # last terminal from token
+                next_state = afnd.terminal_insert_tail(terminal, current_state=next_state)
+            else:                     # middle terminal from token
+                next_state = afnd.terminal_insert_middle(terminal, current_state=next_state)
+                # print(next_state)
+
+            afnd.show_table()
+            print(f"iteration number {i}")
+
 
 
 if __name__ == "__main__":
